bdcda69bfb1f393203c1c77307417ddb3b44bb5c
ritzle <vladislav.ryazanov.study@gmail.com>
2024-05-23 00:21:15 +0700

    number 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # laba6
+
+g++ -o task1 task1.cpp -lncurses для компиляции
+!должна быть установленна библиотека ncurses
--- task1.cpp
+++ task1.cpp
@@ -1,23 +1,14 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <string>
-#include <ncurses.h>
-#include <unistd.h>
-
+#include "playLife.h"
 using namespace std;
 
 template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &matrix)
+ostream &operator<<(ostream &os, const vector<std::vector<T>> &matrix)
 {
     for (const auto &row : matrix)
     {
         for (const auto &element : row)
         {
-            os << element << "\t";
+            os << element << " ";
         }
         os << "\n";
     }
@@ -25,7 +16,7 @@ std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &ma
 }
 
 template <typename T>
-ostream &operator<<(std::ostream &os, const vector<T> &vector)
+ostream &operator<<(ostream &os, const vector<T> &vector)
 {
     for (const auto &element : vector)
     {
@@ -36,16 +27,10 @@ ostream &operator<<(std::ostream &os, const vector<T> &vector)
 
 pair<int, vector<int>> task1(vector<vector<int>> &matrix, int lines, int columns);
 vector<int> task2(vector<vector<int>> &matrix);
+void task3();
 
 void generateMatrix(vector<vector<int>> &matrix, int sizeMatrix);
 void generateMatrix(vector<vector<int>> &matrix, int lines, int columns);
-
-void generatePlayingField(vector<vector<int>> &matrix, int lines, int columns);
-void task3();
-void fieldDisplay(const vector<std::vector<int>> &matrix, int lines, int columns);
-int cellCounting(vector<vector<int>> &matrix, int i, int j, int lines, int columns);
-void updateMatrix(vector<vector<int>> &matrix, int lines, int columns);
-void playLife(vector<vector<int>> &matrix, int lines, int columns);
 
 int main()
 {
@@ -57,7 +42,7 @@ int main()
     pair<int, vector<int>> resultTask1;
     vector<int> resultTask2;
 
-    cout << "1 2 3 E = exit" << endl;
+    cout << "1 2 3 q = exit" << endl;
     while (true)
     {
         cout << endl;
@@ -99,7 +84,7 @@ int main()
             cout << endl;
             break;
 
-        case 'E':
+        case 'q':
             return 0;
         default:
             cout << "unknown team\n";
@@ -163,13 +148,14 @@ void task3() // X ИГРА В ЖИЗНЬ
 {
     char action;
     int sizeMatrix;
+    pair<int, int> lineRow;
 
-    int line = 50;
-    int column = 50;
+    int line = 101;
+    int column = 151;
 
     vector<vector<int>> playingField(line, vector<int>(column, 0));
 
-    cout << "1-random 2-train 3 E = exit" << endl;
+    cout << "1-random 2-train q = exit" << endl;
     cout << "Enter action: ";
     cin >> action;
 
@@ -182,121 +168,16 @@ void task3() // X ИГРА В ЖИЗНЬ
         break;
 
     case '2':
+        lineRow = readingPlayingField(playingField);
+
+        // cout << playingField << endl;
+        // cout << lineRow.first << " " << lineRow.second;
+        playLife(playingField, lineRow.first, lineRow.second);
         break;
 
-    case 'E':
+    case 'q':
         return;
     default:
         cout << "unknown team\n";
-    }
-}
-
-void generatePlayingField(vector<vector<int>> &matrix, int lines, int columns)
-{
-    std::srand(time(nullptr));
-    for (int i = 0; i < lines; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            matrix[i][j] = rand() % 2; // 0 или 1
-        }
-    }
-}
-
-void playLife(vector<vector<int>> &matrix, int lines, int columns)
-{
-    initscr();   // Инициализация ncurses
-    curs_set(0); // Скрываем курсор
-    cbreak();    // Режим постоянного ввода
-    noecho();    // Отключаем вывод введенных символов
-
-    while (true)
-    {
-        fieldDisplay(matrix, lines, columns);
-        updateMatrix(matrix, lines, columns);
-
-        refresh();      // Обновляем экран
-        usleep(100000); // Задержка 100 мс
-    }
-
-    endwin(); // Завершаем работу с ncurses
-}
-
-void updateMatrix(vector<vector<int>> &matrix, int lines, int columns)
-{
-    vector<vector<int>> newMatrix(lines, vector<int>(columns, 0)); // Инициализируем newMatrix
-
-    for (int i = 0; i < lines; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            int numbeLivingCells = cellCounting(matrix, i, j, lines, columns);
-
-            if (matrix[i][j] == 1)
-            { // Если клетка живая
-                if (numbeLivingCells < 2 || numbeLivingCells > 3)
-                { // Умирает от одиночества или перенаселения
-                    newMatrix[i][j] = 0;
-                }
-                else
-                { // Остается живой
-                    newMatrix[i][j] = 1;
-                }
-            }
-            else
-            { // Если клетка мертвая
-                if (numbeLivingCells == 3)
-                { // Оживает
-                    newMatrix[i][j] = 1;
-                }
-                else
-                { // Остается мертвой
-                    newMatrix[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    matrix = move(newMatrix); // Заменяем содержимое matrix на newMatrix
-}
-
-int cellCounting(vector<vector<int>> &matrix, int i, int j, int lines, int columns)
-{
-    int sum = 0;
-
-    // Проверяем соседей клетки (i, j)
-    for (int x = max(0, i - 1); x <= min(i + 1, lines - 1); x++)
-    {
-        for (int y = max(0, j - 1); y <= min(j + 1, columns - 1); y++)
-        {
-            if (x != i || y != j)
-            { // Исключаем саму клетку (i, j)
-                sum += matrix[x][y];
-            }
-        }
-    }
-
-    return sum;
-}
-
-void fieldDisplay(const vector<vector<int>> &matrix, int lines, int columns)
-{
-
-    clear();
-    // Выводим матрицу в новом консольном окне
-    for (int i = 0; i < lines; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            if (matrix[i][j])
-            {
-                mvaddch(i, j, ACS_BLOCK); // Отображаем живую клетку
-            }
-            else
-            {
-                mvaddch(i, j, ' '); // Отображаем мертвую клетку
-            }
-        }
-        cout << endl;
     }
 }
